9dcf37ef0ba38ffaf2c018f65a65fe29c2ab1d22
Pascal Grimaud <pascalgrimaud@users.noreply.github.com>
2018-11-05 06:06:29 +0100

    Fix folder

--- test-integration/scripts/20-no-memory-limit-elasticsearch.sh
+++ test-integration/scripts/20-no-memory-limit-elasticsearch.sh
@@ -3,6 +3,7 @@
 set -e
 source $(dirname $0)/00-init-env.sh
 
+cd "$JHI_FOLDER_APP"
 if [ -a src/main/docker/elasticsearch.yml ]; then
     sed -i -e 's/        environment://1;' src/main/docker/elasticsearch.yml
     sed -i -e 's/            - \"ES_JAVA_OPTS=-Xms1024m -Xmx1024m\"//1;' src/main/docker/elasticsearch.yml
