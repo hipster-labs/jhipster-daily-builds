2b03778e590c0843269036a7a957f519554ea4ed
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-11-14 21:54:45 +0100

    Remove tty for Docker script

--- test-integration/scripts/26-tests-docker.sh
+++ test-integration/scripts/26-tests-docker.sh
@@ -7,13 +7,13 @@ set -e
 #-------------------------------------------------------------------------------
 docker container run -d -e JHI_DISABLE_WEBPACK_LOGS=true --name jhipster jhipster/jhipster:master
 docker container ps
-docker container exec -it jhipster npm --version
-docker container exec -it jhipster yarn --version
-docker container exec -it jhipster yo --version
-docker container exec -it jhipster jhipster --help --no-insight
-docker container exec -it jhipster jhipster info --no-insight
-docker container exec -it jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/master/test-integration/samples/ngx-default/.yo-rc.json -o .yo-rc.json
-docker container exec -it jhipster ls -al
-docker container exec -it jhipster jhipster --force --no-insight --skip-checks --with-entities
-docker container exec -it jhipster ./mvnw test
-docker container exec -it jhipster npm test
+docker container exec -i jhipster npm --version
+docker container exec -i jhipster yarn --version
+docker container exec -i jhipster yo --version
+docker container exec -i jhipster jhipster --help --no-insight
+docker container exec -i jhipster jhipster info --no-insight
+docker container exec -i jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/master/test-integration/samples/ngx-default/.yo-rc.json -o .yo-rc.json
+docker container exec -i jhipster ls -al
+docker container exec -i jhipster jhipster --force --no-insight --skip-checks --with-entities
+docker container exec -i jhipster ./mvnw test
+docker container exec -i jhipster npm test
