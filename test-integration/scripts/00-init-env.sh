3958fc3767a35fadb6936bdefcea41c6551f063a
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-05-25 23:56:35 +0200

    Fix OpenJDK 11 with Azure

--- test-integration/scripts/00-init-env.sh
+++ test-integration/scripts/00-init-env.sh
@@ -33,3 +33,8 @@ JHI_FOLDER_APP="$HOME"/app
 
 # folder for uaa app
 JHI_FOLDER_UAA="$HOME"/uaa
+
+# set correct OpenJDK version
+if [[ "$JHI_JDK" == "11" ]]; then
+    JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+fi
