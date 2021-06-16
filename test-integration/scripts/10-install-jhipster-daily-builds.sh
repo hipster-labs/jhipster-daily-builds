9d8c050bf9b51057508e69e4040b49978127cf43
Marcelo Shima <marceloshima@gmail.com>
2021-06-15 23:25:28 -0300

    Install generator-jhipster before jhipster-bom.

--- test-integration/scripts/10-install-jhipster-daily-builds.sh
+++ test-integration/scripts/10-install-jhipster-daily-builds.sh
@@ -4,41 +4,6 @@ set -e
 source $(dirname $0)/00-init-env.sh
 
 #-------------------------------------------------------------------------------
-# Install JHipster Dependencies and Server-side library
-#-------------------------------------------------------------------------------
-cd "$HOME"
-if [[ "$JHI_REPO" == *"/jhipster" ]]; then
-    echo "*** jhipster: use local version at JHI_REPO=$JHI_REPO"
-
-    cd "$JHI_HOME"
-    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-
-    ./mvnw -ntp clean install -Dgpg.skip=true
-
-elif [[ "$JHI_LIB_BRANCH" == "release" ]]; then
-    echo "*** jhipster: use release version"
-
-else
-    echo "*** jhipster: JHI_LIB_REPO=$JHI_LIB_REPO with JHI_LIB_BRANCH=$JHI_LIB_BRANCH"
-    git clone "$JHI_LIB_REPO" jhipster
-    cd jhipster
-    if [ "$JHI_LIB_BRANCH" == "latest" ]; then
-        LATEST=$(git describe --abbrev=0)
-        git checkout "$LATEST"
-    elif [ "$JHI_LIB_BRANCH" != "main" ]; then
-        git checkout "$JHI_LIB_BRANCH"
-    fi
-    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-
-    test-integration/scripts/10-replace-version-jhipster.sh
-
-    ./mvnw -ntp clean install -Dgpg.skip=true
-    ls -al ~/.m2/repository/tech/jhipster/jhipster-framework/
-    ls -al ~/.m2/repository/tech/jhipster/jhipster-dependencies/
-    ls -al ~/.m2/repository/tech/jhipster/jhipster-parent/
-fi
-
-#-------------------------------------------------------------------------------
 # Install JHipster Generator
 #-------------------------------------------------------------------------------
 cd "$HOME"
@@ -72,6 +37,41 @@ else
 
     npm ci
     npm install -g "$HOME"/generator-jhipster
+fi
+
+#-------------------------------------------------------------------------------
+# Install JHipster Dependencies and Server-side library
+#-------------------------------------------------------------------------------
+cd "$HOME"
+if [[ "$JHI_REPO" == *"/jhipster" ]]; then
+    echo "*** jhipster: use local version at JHI_REPO=$JHI_REPO"
+
+    cd "$JHI_HOME"
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
+    ./mvnw -ntp clean install -Dgpg.skip=true
+
+elif [[ "$JHI_LIB_BRANCH" == "release" ]]; then
+    echo "*** jhipster: use release version"
+
+else
+    echo "*** jhipster: JHI_LIB_REPO=$JHI_LIB_REPO with JHI_LIB_BRANCH=$JHI_LIB_BRANCH"
+    git clone "$JHI_LIB_REPO" jhipster
+    cd jhipster
+    if [ "$JHI_LIB_BRANCH" == "latest" ]; then
+        LATEST=$(git describe --abbrev=0)
+        git checkout "$LATEST"
+    elif [ "$JHI_LIB_BRANCH" != "main" ]; then
+        git checkout "$JHI_LIB_BRANCH"
+    fi
+    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+
+    test-integration/scripts/10-replace-version-jhipster.sh
+
+    ./mvnw -ntp clean install -Dgpg.skip=true
+    ls -al ~/.m2/repository/tech/jhipster/jhipster-framework/
+    ls -al ~/.m2/repository/tech/jhipster/jhipster-dependencies/
+    ls -al ~/.m2/repository/tech/jhipster/jhipster-parent/
 fi
 
 #-------------------------------------------------------------------------------
