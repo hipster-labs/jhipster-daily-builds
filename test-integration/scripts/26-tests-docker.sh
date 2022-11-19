47b4abd6d6108d6e792e10425778bac1bcf48c2c
Marcelo Shima <marceloshima@gmail.com>
2022-11-18 21:51:44 -0300

    adjust docker-image sample name. (#145)

--- test-integration/scripts/26-tests-docker.sh
+++ test-integration/scripts/26-tests-docker.sh
@@ -19,7 +19,7 @@ docker container exec -i jhipster git clone https://github.com/jhipster/jhipster
 docker container exec -w /home/jhipster/jhipster-bom -i jhipster ./mvnw -ntp clean install -Dgpg.skip=true
 
 # generate sample
-docker container exec -i jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/main/test-integration/samples/ngx-default/.yo-rc.json -o .yo-rc.json
+docker container exec -i jhipster curl https://raw.githubusercontent.com/jhipster/generator-jhipster/main/test-integration/samples/ng-default/.yo-rc.json -o .yo-rc.json
 docker container exec -i jhipster ls -al
 docker container exec -i jhipster jhipster --force --no-insight --skip-checks --with-entities
 docker container exec -i jhipster ls -al /home/jhipster/app/
